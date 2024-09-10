b28f9669df51f036a406dedfff973f88fc7a4ad3
chanduPrakashXgithub <156881241+chanduPrakashXgithub@users.noreply.github.com>
2024-09-10 12:43:18 +0530

    Update apiPath.js

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
-export const API_URl="http://localhost:4000"
-// export const API_URl="https://foodiee-backend-app.onrender.com"
+// export const API_URl="http://localhost:4000"
+export const API_URl="https://final-foodieee-backend.onrender.com"
